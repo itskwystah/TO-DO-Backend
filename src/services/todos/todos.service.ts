e13d2cca670c6facb20b6501348e77d28d898b50
itskwystah <krismarcaida15@gmail.com>
2026-02-18 06:37:44 +0800

    feat: implement CRUD operations for todos service

---
+++ src/services/todos/todos.service.ts
@@ -0,0 +1,23 @@
+import TodoModel, { TodoDocument } from "@/models/todo/todo.model";
+import { ITodo } from "@/types/models/todo.types";
+
+export const createTodo = async (taskData: ITodo): Promise<TodoDocument> => {
+  const task = new TodoModel(taskData);
+  return task.save();
+};
+
+export const updateTodo = async (id: string, payload: Partial<ITodo>) => {
+  return TodoModel.findByIdAndUpdate(id, payload, { new: true });
+};
+
+
+export const getAllTodo = async (): Promise<TodoDocument[]> => {
+  return TodoModel.find();
+};
+export const deleteTodo = async (id: string) => {
+  return await TodoModel.findByIdAndDelete(id);
+};
+
+export const getTodoById = async (id: string): Promise<TodoDocument | null> => {
+  return TodoModel.findById(id);
+};
