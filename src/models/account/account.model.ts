7a378fe121ea7c5ac09e70d80cb59fe848df3e4b
itskwystah <krismarcaida15@gmail.com>
2026-02-18 23:40:42 +0800

    feat: refactor account model to include detailed session schema

--- src/models/account/account.model.ts
+++ src/models/account/account.model.ts
@@ -1,20 +1,63 @@
-import { AcctDocType } from "@/types/models/account.type";
-import { model, Model, Schema } from "mongoose"
+import { AcctDocType, SessionType } from '@/types/models/account.type';
+import { model, Model, Schema } from 'mongoose';
 
-const AcctSchema = new Schema<AcctDocType>(
-    {
-        name: { type: String, required: true },
-        email: { type: String, required: true, unique: true },
-        password: { type: String, required: true },
+const SessionSchema = new Schema<SessionType>(
+  {
+    sid: { type: String, required: true },
+    ip: { type: String },
+    geo: {
+      range: [Number],
+      country: String,
+      region: String,
+      eu: String,
+      timezone: String,
+      city: String,
+      ll: [Number],
+      metro: Number,
+      area: Number,
     },
-    { timestamps: true },
+    userAgent: {
+      browser: {
+        name: String,
+        version: String,
+        type: String,
+      },
+      cpu: {
+        architecture: String,
+      },
+      device: {
+        vendor: String,
+        model: String,
+        type: String,
+      },
+      engine: {
+        name: String,
+        version: String,
+      },
+      os: {
+        name: String,
+        version: String,
+      },
+    },
+    token: { type: String, required: true },
+    expiresAt: { type: Date, required: true },
+  },
+  { _id: true, timestamps: false },
 );
 
-const Acct: Model<AcctDocType> = model<
-    AcctDocType,
-    Model<AcctDocType>
->("accounts", AcctSchema);
+const AcctSchema = new Schema<AcctDocType>(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    sessions: [SessionSchema],
+  },
+  { timestamps: true },
+);
+
+const Acct: Model<AcctDocType> = model<AcctDocType, Model<AcctDocType>>(
+  'accounts',
+  AcctSchema,
+);
 
 export default Acct;
-
-
